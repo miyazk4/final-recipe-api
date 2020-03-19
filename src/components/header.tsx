79b2d60fc14fd78edfa063916b30c40cd845a96a
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-19 13:55:32 +0000

    very basic header with simple style

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="headerContainer">
+        <h1 className="headerLogo">Edamam Api</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
